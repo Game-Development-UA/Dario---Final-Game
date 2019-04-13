756a845e4c4df698ba2151efe5c5ef44947abe32
Dario <darioandrade@email.arizona.edu>
2019-04-13 14:49:28 -0700

    Started to implement a new enemy behaviour
    Implemented a path class, which will contain checkpoints among the path the enemies will move towards. Currently implementing it on my EnemyFinal script

--- Assets/Final Scripts/EnemyFinal.cs
+++ Assets/Final Scripts/EnemyFinal.cs
@@ -7,15 +7,35 @@ public class EnemyFinal : MonoBehaviour
     public string Name;
     public float health;
     public float enemySpeed;
+
+    private float horizontalSpeed;
+    private float verticalSpeed;
+    private Transform currCheckPoint;
+    private Vector3 Direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateCheckPoint(0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.transform.Translate(Direction.x * enemySpeed * Time.deltaTime, Direction.y * enemySpeed * Time.deltaTime, 0f);
         
+    }
+
+    public void UpdateCheckPoint(int numCheckPoint) {
+        if (numCheckPoint < Path.Singleton.checkPoints.Count)
+        {
+            currCheckPoint = Path.Singleton.checkPoints[numCheckPoint];
+        }
+        Direction = Vector3.Normalize(currCheckPoint.position - this.transform.position);
+
+    }
+
+    public void CheckPosition() {
+
     }
 }
---
+++ Assets/Final Scripts/Path.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+
+    public List<Transform> checkPoints = new List<Transform>();
+    public static Path Singleton;
+    // Start is called before the first frame update
+
+    void Awake() {
+        Singleton = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
